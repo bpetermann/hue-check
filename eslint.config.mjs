58fcf657e41455834474a40e2ebba3c6e620aa83
bpetermann <benjamin.petermann@gmx.at>
2025-01-08 21:15:33 +0100

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.test.{js,ts}', '**/__tests__/**/*.{js,ts}'],
+    plugins: ['jest'],
+    extends: ['plugin:jest/recommended'],
+    languageOptions: { globals: globals.jest },
+  },
+];
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+};
